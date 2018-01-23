593228ead9b7b08770235ba71b18af609d00797a
dbilgili <dbilgili@sabanciuniv.edu>
2018-01-23 16:03:00 +0100

    List component updated
    List component converted into stateless functional component

--- src/components/List.js
+++ src/components/List.js
@@ -2,15 +2,13 @@ import React, {Component} from 'react'
 import LoadingHOC from './LoadingHOC'
 import '../styles/main.css'
 
-class List extends Component{
-  render(){
-    const{usernames} = this.props
-    return(
-      <ul>
-        {usernames.map(user => <li key={user.id}>{user.name}</li>)}
-      </ul>
-    )
-  }
+const List = (props) =>{
+  const{usernames} = props
+  return(
+    <ul>
+      {usernames.map(user => <li key={user.id}>{user.name}</li>)}
+    </ul>
+  )
 }
 
 export default LoadingHOC(List)
